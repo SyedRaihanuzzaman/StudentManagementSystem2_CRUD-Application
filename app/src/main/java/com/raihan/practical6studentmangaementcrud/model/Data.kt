7bd8c00eea66c187b48fbe0a520bd7d71bbf3da4
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-11-11 11:55:39 +0600

    First Commit

---
+++ app/src/main/java/com/raihan/practical6studentmangaementcrud/model/Data.kt
@@ -0,0 +1,13 @@
+package com.raihan.practical6studentmangaementcrud.model
+
+import com.google.firebase.Timestamp
+
+data class Data(
+    var id:String?=null,
+    val stuid:String?=null,
+    val name:String?=null,
+    val email:String?=null,
+    val subject:String?=null,
+    val birthday:String?=null,
+    val timestamp: Timestamp?= null
+)
